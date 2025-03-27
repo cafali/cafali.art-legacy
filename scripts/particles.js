9e798090bb0e2e7caf321df4dd0f4dd0a03ef5b6
cafali <77307171+cafali@users.noreply.github.com>
2025-03-27 15:22:14 +0100

    particle effect

---
+++ scripts/particles.js
@@ -0,0 +1,14 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 20, density: { enable: true, value_area: 1600 } },
+    color: { value: "#00ff99" },
+    shape: { type: "triangle" },
+    opacity: { value: 0.5, random: true },
+    size: { value: 9, random: true },
+    move: { enable: true, speed: 1, direction: "none", out_mode: "out" }
+  },
+  interactivity: {
+    events: { onhover: { enable: true, mode: "repulse" } },
+    modes: { repulse: { distance: 100 } }
+  }
+});
